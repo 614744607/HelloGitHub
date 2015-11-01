93c59c8767d275f0d2f7923c18584839f09cbb9f
駒井陽希 <mariogalaxy3kibou@yahoo.co.jp>
2015-11-01 14:41:33 +0900

    first commit

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -8,18 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController  {
+    @IBOutlet var myImage: UIImageView!
+    var imageView = UIImage(named:"っっっs.png")
+    var imageView2 = UIImage(named:"ああああ.png")
+    var number : Int=0
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func plus() {
+        number=number+1
+        label.text = String(number)
+        
+        if number >= 50 {
+            label.textColor=UIColor.redColor()
+            myImage.image=imageView
+            
+        }else{
+            label.textColor=UIColor.blackColor()
+            myImage.image=imageView2
+            
+        }
+    }
+    @IBAction func x2(){
+        number=number*2
+        label.text = String(number)
+        
+        if number >= 50 {
+            label.textColor=UIColor.redColor()
+            myImage.image=imageView
+            
+        }else{
+            label.textColor=UIColor.blackColor()
+            myImage.image=imageView2
+            
+        }
+    }
+    @IBAction func leset() {
+        number = 0
+        label.text = String(number)
+        myImage.image=imageView2
+        label.textColor=UIColor.blackColor()
+    }
 }
-
